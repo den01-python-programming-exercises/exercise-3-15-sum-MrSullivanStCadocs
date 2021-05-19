ec1c65f1c5ba8a4660a667ed3202ea0db4c6e8e8
MrSullivan <>
2021-05-19 21:39:44 +0000

    lots

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,16 @@
 def main():
     #write your code below this line
+    numbers = []
+    numbers.append(3)
+    numbers.append(2)
+    numbers.append(6)
+    numbers.append(-1)
+    print(sum_list(numbers)
+       
+    
+def sum_list(numbers):
+  return sum(numbers)
 
+      
 if __name__ == '__main__':
     main()
